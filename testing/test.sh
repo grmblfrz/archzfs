cdce0ea8655c44c20f0bd782505e75a7fdbcacd5
Jesus Alvarez <jeezusjr@gmail.com>
2016-05-29 01:56:09 -0700

    Add support for arbitrary test configs

--- testing/test.sh
+++ testing/test.sh
@@ -18,24 +18,24 @@ ssh_pass="sshpass -p azfstest"
 ssh="${ssh_pass} ${ssh_cmd}"
 test_pkg_workdir="archzfs"
 archiso_basename=$(basename ${archiso_url})
-base_image_basename="archzfs-base-archiso-${archiso_basename:10:-9}"
 
 
 export packer_work_dir="${script_dir}/packer_work"
 export base_image_output_dir="${packer_work_dir}"
-
-
-run_cmd_show_and_capture_output "find ${script_dir} -iname '*archzfs-base-*' -printf \"%P\\n\" | sort -r | head -n 1"
-if [[ ${run_cmd_output} == "" ]]; then
-    export base_image_name="${base_image_basename}-build-$(date +%Y.%m.%d).qcow2"
-else
-    export base_image_name="${run_cmd_output}"
-fi
-
-
-export base_image_path="${script_dir}/${base_image_name}"
-export work_image_randname="${base_image_name%.qcow2}_${RANDOM}.qcow2"
 export PACKER_CACHE_DIR="packer_cache"
+
+
+gen_base_image_name() {
+    base_image_basename="$(basename ${test_mode})-archiso-${archiso_basename:10:-9}"
+    run_cmd_output=$(find ${script_dir} -iname "*$(basename ${test_mode})-*" -printf "%P\\n" | sort -r | head -n 1)
+    if [[ ${run_cmd_output} == "" ]]; then
+        export base_image_name="${base_image_basename}-build-$(date +%Y.%m.%d).qcow2"
+    else
+        export base_image_name="${run_cmd_output}"
+    fi
+    export base_image_path="${script_dir}/${base_image_name}"
+    export work_image_randname="${base_image_name%.qcow2}_${RANDOM}.qcow2"
+}
 
 
 usage() {
@@ -60,26 +60,23 @@ usage() {
     echo
     echo "Commands:"
     echo
-    echo "    base   Build the base image."
-    echo "    test   Build test packages."
+    for ml in "${test_commands_list[@]}"; do
+        mn=$(basename ${ml})
+        echo -e "    ${mn#archzfs-qemu-}"
+    done
     exit 155
 }
+
+
+generate_test_commands_list
+debug_print_array "test_commands_list" "${test_commands_list[@]}"
 
 
 generate_mode_list "${script_dir}/../src/kernels"
 
 
-if [[ $# -lt 1 ]]; then
-    usage
-fi
-
-
 for (( a = 0; a < $#; a++ )); do
-    if [[ ${args[$a]} == "base" ]]; then
-        commands+=("base")
-    elif [[ ${args[$a]} == "test" ]]; then
-        commands+=("test")
-    elif [[ ${args[$a]} == "-R" ]]; then
+    if [[ ${args[$a]} == "-R" ]]; then
         commands+=("reuse")
     elif [[ ${args[$a]} == "-n" ]]; then
         dry_run=1
@@ -88,10 +85,23 @@ for (( a = 0; a < $#; a++ )); do
     elif [[ ${args[$a]} == "-h" ]]; then
         usage
     else
-        check_mode "${args[$a]}"
+        check_test_mode "${args[$a]}"
         debug "have mode '${mode}'"
+        debug "have test mode '${test_mode}'"
     fi
 done
+
+
+gen_base_image_name
+
+
+if [[ $# -lt 1 ]]; then
+    usage
+elif [[ ${mode} == "" || ${test_mode} == "" ]]; then
+    echo
+    error "A build mode and test command must be selected!"
+    usage
+fi
 
 
 if [[ ${debug_flag} -eq 1 ]]; then
@@ -119,7 +129,9 @@ copy_latest_packages() {
 }
 
 
-if have_command "base"; then
+# if have_command "base"; then
+if [[ "${test_mode}" != "" ]]; then
+
     msg "Building arch base image"
 
     if [[ -d "${packer_work_dir}/output-qemu" ]]; then
@@ -140,62 +152,62 @@ if have_command "base"; then
     msg2 "Using packer to build the base image ..."
 
     # Uncomment to enable packer debug
-    # export PACKER_LOG=1
+    export PACKER_LOG=1
 
-    run_cmd "check_symlink '${script_dir}/arch-zfs-base/setup-test-image.sh' '${packer_work_dir}/setup-test-image.sh'"
+    run_cmd "check_symlink '${test_mode}/setup.sh' '${packer_work_dir}/setup.sh'"
     run_cmd "check_symlink '${script_dir}/poweroff.timer' '${packer_work_dir}/poweroff.timer'"
-    run_cmd "cd ${packer_work_dir} && packer-io build -debug ${script_dir}/arch-zfs-base/arch-zfs-base.json"
+    run_cmd "cd ${packer_work_dir} && packer-io build -debug ${test_mode}/packer.json"
 
     msg "Moving the compiled base image"
     run_cmd "mv -f ${base_image_output_dir}/output-qemu/packer-qemu ${base_image_path}"
 fi
 
 
-if have_command "test";  then
-    msg "Testing package target '${mode}'"
+# if have_command "test";  then
+    # msg "Testing package target '${mode}'"
 
-    if ! have_command "reuse"; then
-        msg2 "Building test packages"
-        build_test_packages
-    fi
+    # if ! have_command "reuse"; then
+        # msg2 "Building test packages"
+        # build_test_packages
+    # fi
 
-    msg2 "Copying test packages"
-    copy_latest_packages
+    # msg2 "Copying test packages"
+    # copy_latest_packages
 
-    msg2 "Cloning ${base_image_path}"
-    run_cmd "cp ${base_image_path} ${work_image_randname}"
+    # msg2 "Cloning ${base_image_path}"
+    # run_cmd "cp ${base_image_path} ${work_image_randname}"
 
-    msg "Booting VM clone..."
-    cmd="qemu-system-x86_64 -enable-kvm "
-    cmd+="-m 4096 -smp 2 -redir tcp:2222::22 -drive "
-    cmd+="file=${work_image_randname},if=virtio"
-    run_cmd "${cmd}" &
+    # msg "Booting VM clone..."
+    # cmd="qemu-system-x86_64 -enable-kvm "
+    # cmd+="-m 4096 -smp 2 -redir tcp:2222::22 -drive "
+    # cmd+="file=${work_image_randname},if=virtio"
+    # run_cmd "${cmd}" &
 
-    if [[ -z "${debug_flag}" ]]; then
-        msg "Waiting for SSH..."
-        while :; do
-            run_cmd "${ssh} root@localhost echo &> /dev/null"
-            if [[ ${run_cmd_return} -eq 0 ]]; then
-                break
-            fi
-        done
-    fi
+    # if [[ -z "${debug_flag}" ]]; then
+        # msg "Waiting for SSH..."
+        # while :; do
+            # run_cmd "${ssh} root@localhost echo &> /dev/null"
+            # if [[ ${run_cmd_return} -eq 0 ]]; then
+                # break
+            # fi
+        # done
+    # fi
 
-    msg2 "Copying the latest packages to the VM"
-    copy_latest_packages
-    run_cmd "rsync -vrthP -e '${ssh}' archzfs/x64/ root@localhost:"
-    run_cmd "${ssh} root@localhost pacman -U --noconfirm '*.pkg.tar.xz'"
+    # msg2 "Copying the latest packages to the VM"
+    # copy_latest_packages
+    # run_cmd "rsync -vrthP -e '${ssh}' archzfs/x64/ root@localhost:"
+    # run_cmd "${ssh} root@localhost pacman -U --noconfirm '*.pkg.tar.xz'"
 
-    # msg2 "Cloning ZFS test suite"
-    # run_cmd "${ssh} root@localhost git clone https://github.com/zfsonlinux/zfs-test.git /usr/src/zfs-test"
-    # run_cmd "${ssh} root@localhost chown -R zfs-tests: /usr/src/zfs-test/"
+    # # msg2 "Cloning ZFS test suite"
+    # # run_cmd "${ssh} root@localhost git clone https://github.com/zfsonlinux/zfs-test.git /usr/src/zfs-test"
+    # # run_cmd "${ssh} root@localhost chown -R zfs-tests: /usr/src/zfs-test/"
 
-    # msg2 "Building ZFS test suite"
-    # run_cmd "${ssh} root@localhost 'cd /usr/src/zfs-test && ./autogen.sh && ./configure'"
-    # run_cmd "${ssh} root@localhost 'cd /usr/src/zfs-test && ./autogen.sh && ./configure && make test'"
+    # # msg2 "Building ZFS test suite"
+    # # run_cmd "${ssh} root@localhost 'cd /usr/src/zfs-test && ./autogen.sh && ./configure'"
+    # # run_cmd "${ssh} root@localhost 'cd /usr/src/zfs-test && ./autogen.sh && ./configure && make test'"
 
-    # msg2 "Cause I'm housin"
-    # run_cmd "${ssh} root@localhost systemctl poweroff &> /dev/null"
+    # # msg2 "Cause I'm housin"
+    # # run_cmd "${ssh} root@localhost systemctl poweroff &> /dev/null"
 
-    # wait
-fi
+    # # wait
+# fi
